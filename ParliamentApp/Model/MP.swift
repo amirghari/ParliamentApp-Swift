5f579bcd849da38bbd180b87e7aad0f90f6d5300
Amir Ghari <amir.ghari@metropolia.fi>
2024-11-18 10:57:10 +0200

    MP is updated

--- ParliamentApp/Model/MP.swift
+++ ParliamentApp/Model/MP.swift
@@ -1,15 +1,27 @@
 import Foundation
+import SwiftData
 
-struct MP: Identifiable, Codable {
-    let id: Int
-    let seatNumber: Int
-    let lastName: String
-    let firstName: String
-    let party: String
-    let bornYear: Int
-    let constituency: String
+@Model
+class MP {
+    var id: Int
+    var seatNumber: Int
+    var lastName: String
+    var firstName: String
+    var party: String
+    var bornYear: Int
+    var constituency: String
     var isFavoriteMember: Bool = false
-
+    
+    init(id: Int, seatNumber: Int, lastName: String, firstName: String, party: String, bornYear: Int, constituency: String, isFavoriteMember: Bool) {
+        self.id = id
+        self.seatNumber = seatNumber
+        self.lastName = lastName
+        self.firstName = firstName
+        self.party = party
+        self.bornYear = bornYear
+        self.constituency = constituency
+        self.isFavoriteMember = isFavoriteMember
+    }
     enum CodingKeys: String, CodingKey {
         case id = "personNumber"
         case seatNumber
--- ParliamentApp/ParliamentAppApp.swift
+++ ParliamentApp/ParliamentAppApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ParliamentAppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: MP.self)
         }
     }
 }
